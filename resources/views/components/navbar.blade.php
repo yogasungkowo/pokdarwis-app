931754132a2fd394413c01d771c2ac835459956a
yogasungkowo <prayogasungkowo12@gmail.com>
2025-08-01 21:39:52 +0700

    Add main layout and components for POKDARWIS website
    - Created app.blade.php for main application layout.
    - Added footer.blade.php as a placeholder for footer content.
    - Implemented meta.blade.php for meta tags and page title.
    - Developed navbar.blade.php for site navigation with links.
    - Established main.blade.php layout integrating Bootstrap and components.
    - Designed index.blade.php for the main page with sections for welcome, activities, and articles, including custom styles and scripts.

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,42 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid sticky-top" style="width: 85%">
+    <a class="navbar-brand" href="#">
+        <img src="{{ asset('img/logo.jpg') }}" alt="Logo POKDARWIS" style="max-width: 50px; border-radius: 50%; padding-right: 10px;">
+        <span class="fw-semibold fs-5 text-dark">POKDARWIS</span>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center w-100 text-dark">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tentang Kami</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Artikel</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Kontak Kami</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <x-meta />
+    
+    <!-- Bootstrap 5.3 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+    @stack('styles')
+</head>
+<body>
+    <x-navbar />
+    @yield('content')
+
+    @stack('scripts')
+</body>
+</html>
